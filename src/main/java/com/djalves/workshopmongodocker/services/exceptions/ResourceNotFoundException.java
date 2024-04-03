f88e0bc7621b474b6fc5a2501f017c8134198c23
Diego Alves <djalves424@gmail.com>
2024-04-03 20:35:00 -0300

    Created ResourceNotFoundException error message class

---
+++ src/main/java/com/djalves/workshopmongodocker/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.djalves.workshopmongodocker.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+}
