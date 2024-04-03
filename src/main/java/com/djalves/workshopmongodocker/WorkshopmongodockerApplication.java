cc3e0218285a84cd3f53aaefd6060f3fee4bbaf2
Diego Alves <djalves424@gmail.com>
2024-04-03 20:20:00 -0300

    Project created

---
+++ src/main/java/com/djalves/workshopmongodocker/WorkshopmongodockerApplication.java
@@ -0,0 +1,12 @@
+package com.djalves.workshopmongodocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongodockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongodockerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/djalves/workshopmongodocker/WorkshopmondodockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djalves.workshopmongodocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmondodockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
