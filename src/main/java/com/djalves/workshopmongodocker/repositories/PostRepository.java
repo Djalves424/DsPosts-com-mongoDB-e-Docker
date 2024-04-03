eb7f16e9d63cba103d816967c02d089372f64552
Diego Alves <djalves424@gmail.com>
2024-04-03 20:32:21 -0300

    PostRepository interface created

---
+++ src/main/java/com/djalves/workshopmongodocker/repositories/PostRepository.java
@@ -0,0 +1,21 @@
+package com.djalves.workshopmongodocker.repositories;
+
+import com.djalves.workshopmongodocker.models.entities.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
+import java.util.List;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ $and: [ { moment: {$gte: ?1} }, { moment: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    List<Post> fullSearch(String text, Instant startMoment, Instant endMoment);
+}
