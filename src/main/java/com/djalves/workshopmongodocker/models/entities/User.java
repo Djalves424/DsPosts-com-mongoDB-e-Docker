03a1f7c339ba57523a96e1dae55d32291377b314
Diego Alves <djalves424@gmail.com>
2024-04-03 20:22:35 -0300

    Created the user class in the entities layer

---
+++ src/main/java/com/djalves/workshopmongodocker/models/entities/User.java
@@ -0,0 +1,58 @@
+package com.djalves.workshopmongodocker.models.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+
+    @DBRef(lazy = true)
+    public List<Post> posts = new ArrayList<>();
+
+    public User() {
+
+    }
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+}
